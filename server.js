db22a4a50a02c46da3f0845467227011e890d28f
hawwinrmdhn67 <hawwinxstar@gmail.com>
2025-08-30 21:27:41 +0700

    add serve react build to server.js

--- server.js
+++ server.js
@@ -163,6 +163,15 @@ app.get("/api/favorites/:google_id", async (req, res) => {
   }
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
